974feb802e246cbc0bf91a89bf706c124f80eafb
PathangeNandini <pathangenandini@gmail.com>
2026-07-05 21:46:02 +0530

    login page

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -2,6 +2,7 @@ export default function Login() {
     return (
         <div>
             <h1>Login Page</h1>
+            <p>Not registered yet? <a href="/register">Register here</a></p>
         </div>
     );
 }
